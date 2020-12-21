d0f2e06924954e735418aeb4d757b08a7de42d37
Marek Chlabicz <marek.chlabicz@gmail.com>
2020-12-21 21:55:49 +0100

    MainExtended

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main (String[] arg) {
+        String myName = "Marek";
+        int myAge = 44;
+        double height = 1.76;
+        char carClass = 'C';
+        boolean isBeginnerProgrammer = true;
+        String myHome = "Lodz";
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isBeginnerProgrammer);
+        System.out.println(myHome);
+    }
+}
